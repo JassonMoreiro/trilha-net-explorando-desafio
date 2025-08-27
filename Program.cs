76e476dbab767c484aa449496036cdc8297aad7c
Jasson Moreira <jassonmoreiro@gmail.com>
2025-08-27 19:05:34 -0300

    Desafio DIO

--- Program.cs
+++ Program.cs
@@ -5,21 +5,50 @@ Console.OutputEncoding = Encoding.UTF8;
 
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
+Pessoa p1 = new Pessoa(nome: "Jasson Hadryell");
+Pessoa p2 = new Pessoa(nome: "Lavinia Orbolato");
+Pessoa p3 = new Pessoa(nome: "Ana Clara");
+Pessoa p4 = new Pessoa(nome: "João Vitor");
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+// Reserva para a suíte Premium com p1 e p2
+List<Pessoa> hospedesPremium = new List<Pessoa> { p1, p2 };
+Suite suitePremium = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Reserva reservaPremium = new Reserva(diasReservados: 5);
+reservaPremium.CadastrarSuite(suitePremium);
+reservaPremium.CadastrarHospedes(hospedesPremium);
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+// Reserva para a suíte Master com p3
+List<Pessoa> hospedesMaster = new List<Pessoa> { p3 };
+Suite suiteMaster = new Suite(tipoSuite: "Master", capacidade: 5, valorDiaria: 80);
+Reserva reservaMaster = new Reserva(diasReservados: 3);
+reservaMaster.CadastrarSuite(suiteMaster);
+reservaMaster.CadastrarHospedes(hospedesMaster);
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+List<Pessoa> hospedesPresidencial = new List<Pessoa> { p4 };
+Suite suitePresidencial = new Suite(tipoSuite: "Presidencial", capacidade: 10, valorDiaria: 150);
+Reserva reservaPresidencial = new Reserva(diasReservados: 7);
+reservaPresidencial.CadastrarSuite(suitePresidencial);
+reservaPresidencial.CadastrarHospedes(hospedesPresidencial);
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+Console.WriteLine($"Hóspedes na Suíte {reservaPremium.Suite.TipoSuite}");
+foreach (var hospede in hospedesPremium)
+{
+    Console.WriteLine(hospede.Nome);
+}
+Console.WriteLine($"O valor de sua diária é {reservaPremium.CalcularValorDiaria()} R$, Volte Sempre!");
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+// Exibe a mensagem para a suíte Master
+Console.WriteLine($"Hóspedes na Suíte {reservaMaster.Suite.TipoSuite}");
+foreach (var hospede in hospedesMaster)
+{
+    Console.WriteLine(hospede.Nome);
+}
+Console.WriteLine($"O valor de sua diária é {reservaMaster.CalcularValorDiaria()} R$, Volte Sempre!");
+
+// Exibe a mensagem para a suíte Presidencial
+Console.WriteLine($"Hóspedes na Suíte {reservaPresidencial.Suite.TipoSuite}");
+foreach (var hospede in hospedesPresidencial)
+{
+    Console.WriteLine(hospede.Nome);
+}
+Console.WriteLine($"O valor de sua diária é {reservaPresidencial.CalcularValorDiaria()} R$, Volte Sempre!");
